06905b2222fbfb00e9987f1b6181fc3744048fda
Cameron Murphy <camurphy@gmail.com>
2019-03-27 13:54:29 +1100

    Use two space indent

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ var _ = require('lodash');
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    .setOutputPath('public/build/')
-    .setPublicPath('/build')
-    .addEntry('app', './assets/js/app.js')
-    .addEntry('security', './assets/js/security.js')
-    .enableSingleRuntimeChunk()
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-    .enableSassLoader()
-    .configureBabel(function(babelConfig) {
-        const presetEnvPreset = _.find(babelConfig.presets, element => _.includes(element, '@babel/preset-env'));
-        const presetEnvConfig = _.find(presetEnvPreset, _.isObject);
-        _.set(presetEnvConfig, 'corejs', '3');
-    })
+  .setOutputPath('public/build/')
+  .setPublicPath('/build')
+  .addEntry('app', './assets/js/app.js')
+  .addEntry('security', './assets/js/security.js')
+  .enableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .enableSassLoader()
+  .configureBabel(function(babelConfig) {
+    const presetEnvPreset = _.find(babelConfig.presets, element => _.includes(element, '@babel/preset-env'));
+    const presetEnvConfig = _.find(presetEnvPreset, _.isObject);
+    _.set(presetEnvConfig, 'corejs', '3');
+  })
 ;
 
 module.exports = Encore.getWebpackConfig();
