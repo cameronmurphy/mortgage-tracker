dc005a2d1c45480bf29659bae9e56ab06d4264c1
Cameron Murphy <camurphy@gmail.com>
2019-04-01 15:36:07 +1100

    Upgraded JS dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,6 @@ Encore
   .enableSourceMaps(!Encore.isProduction())
   .enableVersioning(Encore.isProduction())
   .enableSassLoader()
-  .configureBabel(function(babelConfig) {
-    const presetEnvPreset = _.find(babelConfig.presets, element => _.includes(element, '@babel/preset-env'));
-    const presetEnvConfig = _.find(presetEnvPreset, _.isObject);
-    _.set(presetEnvConfig, 'corejs', '3');
-  })
 ;
 
 module.exports = Encore.getWebpackConfig();
